78799a9c5d72850590ae4f71532eb14efbc35478
Josh Johnson <josh@joshajohnson.com>
2020-05-03 21:07:31 +1000

    qui working!

--- software/SignalGenerator.py
+++ software/SignalGenerator.py
@@ -1,7 +1,8 @@
+import argparse
 import serial
 from serial.tools import list_ports
 import io
-import argparse
+
 
 class SignalGenerator:
     def __init__(self):
@@ -17,7 +18,7 @@ class SignalGenerator:
 
         # Argparse
         self.parser = None
-        self.user_input = [] 
+        self.user_input = []
         self.args = None
 
         # Command line args
@@ -28,6 +29,9 @@ class SignalGenerator:
         self.sweep_stop = 100
         self.sweep_steps = 77
         self.sweep_time = 5
+        self.led_display = "kitt"
+        self.rf_enabled = False
+        self.updated = False
 
     def find_serial(self):
         """ Find a Signal Generator """
@@ -45,17 +49,17 @@ class SignalGenerator:
         else:
             print("Signal Generator found on port: {}".format(self.com_port))
 
-
-    def connect_serial(self, com_port = None):
+    def connect_serial(self, com_port=None):
         """ Connect to a Signal Generator """
 
         # Connect to given COM port
         if com_port is not None:
             self.com_port = com_port
-        
+
         try:
-            self.serial = serial.Serial(self.com_port, 115200, timeout = 0.1)
-            self.serial_io = io.TextIOWrapper(io.BufferedRWPair(self.serial, self.serial))
+            self.serial = serial.Serial(self.com_port, 115200, timeout=0.1)
+            self.serial_io = io.TextIOWrapper(
+                io.BufferedRWPair(self.serial, self.serial))
 
             self.send_data("WHOAMI")
             self.serial_io.flush()
@@ -68,48 +72,56 @@ class SignalGenerator:
         except:
             print("Could not connect.")
 
-
-    def connect(self, com_port = None):
+    def connect(self, com_port=None):
         """ Finds and Connects to Signal Generator """
         if self.com_port == None:
             self.find_serial()
         self.connect_serial(com_port)
-        
 
     def send_data(self, data):
         """ Sends data to Signal Generator """
+        print("+++ " + data)
         self.serial_io.write(data + "\r\n")
         self.serial_io.flush()
 
-
-    def get_data(self, type = "user"):
+    def get_data(self, type=None):
         """ Gets multiple user directed lines from Signal Generator, until timeout is hit """
-        raw_recv = self.serial_io.readlines()
+
         self.serial_user_data = []
         self.serial_rf_data = []
-        self.serial_log_data = []
-        for line in raw_recv:
-            if line[0] == ">":
-                self.serial_user_data.append(line.rstrip())
-            if line[0] == "+":
-                self.serial_log_data.append(line.rstrip())
-            if line[0] == "?":
-                self.serial_rf_data.append(line.rstrip())
-    
+
+        raw_recv = self.serial_io.readline()
+
+        while (raw_recv != ""):
+
+            if raw_recv[0] == ">":
+                self.serial_user_data.append(raw_recv.rstrip())
+                if type == "user":
+                    print(self.serial_user_data[-1])
+            if raw_recv[0] == "+":
+                self.serial_log_data.append(raw_recv.rstrip())
+                if type == "log":
+                    print(self.serial_log_data[-1])
+            if raw_recv[0] == "?":
+                self.serial_rf_data.append(raw_recv.rstrip())
+                if type == "rf":
+                    print(self.serial_rf_data[-1])
+
+            raw_recv = self.serial_io.readline()
+
         if type == "user":
             return self.serial_user_data
         if type == "log":
             return self.serial_log_data
         if type == "rf":
             return self.serial_log_data
-            
 
     def talk(self):
         """ Transparently opens a COM port between user and Signal Generator """
 
         if not self.connected:
             self.connect()
-        
+
         if self.connected:
             while True:
                 user_input = input()
@@ -117,9 +129,9 @@ class SignalGenerator:
                     break
                 self.send_data(user_input)
                 self.get_data()
-                [print(line) for line in self.serial_data]
+                [print(line) for line in self.serial_user_data]
 
-    def config_sig_gen(self, frequency = None, power = None):
+    def config_sig_gen(self, frequency=None, power=None):
         if frequency is None:
             frequency = self.frequency
         if power is None:
@@ -127,8 +139,7 @@ class SignalGenerator:
 
         self.send_data("sigGen({},{})".format(frequency, power))
 
-    def config_sweep(self, start_freq = None, stop_freq = None, power = None, sweep_steps = None, time = None):
-
+    def config_sweep(self, start_freq=None, stop_freq=None, power=None, sweep_steps=None, time=None):
         if start_freq is None:
             start_freq = self.sweep_start
         if stop_freq is None:
@@ -140,41 +151,51 @@ class SignalGenerator:
         if time is None:
             time = self.sweep_time
 
-        self.send_data("sweep({},{},{},{},{})".format(start_freq, stop_freq, power, sweep_steps, time))
+        self.send_data("sweep({},{},{},{},{})".format(
+            start_freq, stop_freq, power, sweep_steps, time))
 
-    def config_RF(self, enabled = False):
+    def config_RF(self, enabled=False):
         if enabled:
+            self.rf_enabled = True
             self.send_data("enableRF")
         else:
+            self.rf_enabled = False
             self.send_data("disableRF")
 
-    def config_leds(self, led_display = None):
-        
+    def config_leds(self, led_display=None):
         available_displays = ["kitt", "binary", "rainbow"]
-        
-        if led_display in available_displays:
-            self.send_data("{}".format(led_display))
+
+        if led_display == "off":
+            self.send_data("led 0")
         else:
-            print("LED Display not found.")
+            if led_display in available_displays:
+                self.send_data("{}".format(led_display))
+            else:
+                print("LED option not found.")
 
     def parse_inputs(self, user_input):
 
-        self.parser = argparse.ArgumentParser(description="Controls RF Signal Generator")
+        self.parser = argparse.ArgumentParser(
+            description="Controls RF Signal Generator")
 
         self.parser.add_argument("-m", "--mode",        type=str,   dest="mode",
-                            help="select mode ([g]enerator, s[w]eep)")
-        self.parser.add_argument("-f, ""--frequency",   type=float, dest="frequency",    
-                            help="frequency for signal generator in MHz")
-        self.parser.add_argument("-p, ""--power",       type=float, dest="power",   
-                            help="output power in dBm")
-        self.parser.add_argument("-a, ""--start",       type=float, dest="start_freq",  
-                            help="start frequency for sweep in MHz")
-        self.parser.add_argument("-o, ""--stop",        type=float, dest="stop_freq",    
-                            help="stop frequency for sweep in MHz")
-        self.parser.add_argument("-s, ""--steps",       type=int,   dest="steps",   
-                            help="number of steps in sweep")
-        self.parser.add_argument("-t, ""--time",        type=float, dest="time",     
-                            help="time to sweep frequencies in seconds")
+                                 help="select mode ([g]enerator, s[w]eep)")
+        self.parser.add_argument("-f", "--frequency",   type=float, dest="frequency",
+                                 help="frequency for signal generator in MHz")
+        self.parser.add_argument("-p", "--power",       type=float, dest="power",
+                                 help="output power in dBm")
+        self.parser.add_argument("-a", "--start",       type=float, dest="start_freq",
+                                 help="start frequency for sweep in MHz")
+        self.parser.add_argument("-o", "--stop",        type=float, dest="stop_freq",
+                                 help="stop frequency for sweep in MHz")
+        self.parser.add_argument("-s", "--steps",       type=int,   dest="steps",
+                                 help="number of steps in sweep")
+        self.parser.add_argument("-t", "--time",        type=float, dest="time",
+                                 help="time to sweep frequencies in seconds")
+        self.parser.add_argument("-l", "--led",         type=str,   dest="led",
+                                 help="led display (kitt, rainbow, binary)")
+        self.parser.add_argument("-r", "--rf",          type=int,   dest="rf_enabled",
+                                 help="rf enabled (1, 0)")
 
         # parse all the vars
         self.user_input = []
@@ -206,25 +227,28 @@ class SignalGenerator:
         if self.args.time is not None:
             self.sweep_time = self.args.time
 
-if __name__ == '__main__':
-    sig_gen = SignalGenerator()
-    sig_gen.connect()
+        if self.args.led is not None:
+            self.updated = "led"
+            self.led_display = self.args.led
 
-    while True:
+        if self.args.rf_enabled is not None:
+            self.updated = "rf"
+            if self.args.rf_enabled == 1:
+                self.rf_enabled = True
+            elif self.args.rf_enabled == 0:
+                self.rf_enabled = False
 
-        try:
-            user_input = input("$ ")
-            sig_gen.parse_inputs(user_input)
-
-            if sig_gen.mode == "generator":
-                sig_gen.config_sig_gen()
-            if sig_gen.mode == "sweep":
-                sig_gen.config_sweep()
-
-            [print(line) for line in sig_gen.get_data("user")]
-
-        except:
-            pass
-
+        self.send_commands()
         
- 
+    def send_commands(self):
+        if self.updated == "rf":
+            self.config_RF(self.rf_enabled)
+        elif self.updated == "led":
+            self.config_leds(self.led_display)
+        elif self.mode == "generator":
+            self.config_sig_gen()
+        elif self.mode == "sweep":
+            self.config_sweep()
+
+        self.get_data("user")
+        self.updated = False
